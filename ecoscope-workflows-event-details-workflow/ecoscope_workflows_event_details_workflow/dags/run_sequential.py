e4bf1841795ab56d2b2ee83b1d8e398d835a450f
atmorling <atmorling@gmail.com>
2026-09-04 01:43:35 +0200

    Bump ecoscope.platform to 2.20.0 (desktop) (#53)
    * spec update
    
    * recompile

--- ecoscope-workflows-event-details-workflow/ecoscope_workflows_event_details_workflow/dags/run_sequential.py
+++ ecoscope-workflows-event-details-workflow/ecoscope_workflows_event_details_workflow/dags/run_sequential.py
@@ -483,6 +483,7 @@ def main(params: dict[str, Any], validate_params_schema: bool = True):
             field_name_options=["latitude"],
             output_type="str",
             output_column_name="latitude",
+            fan_out=False,
             **(params.get("extract_latitude") or {}),
         )
         .call()
@@ -507,6 +508,7 @@ def main(params: dict[str, Any], validate_params_schema: bool = True):
             field_name_options=["longitude"],
             output_type="str",
             output_column_name="longitude",
+            fan_out=False,
             **(params.get("extract_longitude") or {}),
         )
         .call()
@@ -531,6 +533,7 @@ def main(params: dict[str, Any], validate_params_schema: bool = True):
             field_name_options=["name"],
             output_type="str",
             output_column_name="reported_by_name",
+            fan_out=False,
             **(params.get("extract_reported_by") or {}),
         )
         .call()
